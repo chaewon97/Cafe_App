2d5b7d28c7386eb878c9447889999c061aa5e2b3
kdhooon <conpulake@sju.ac.kr>
2020-06-14 19:59:40 +0900

    하단 네비게이션 완료

---
+++ app/src/main/java/com/example/org/cafe_app/Cafe_List_Fragment.java
@@ -0,0 +1,21 @@
+package com.example.org.cafe_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Beverage_List_Fragment extends Fragment {
+    ViewGroup viewGroup;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.beverage_list_fragment, container, false);
+
+        return viewGroup;
+    }
+}
